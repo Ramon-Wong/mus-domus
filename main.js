b1ec189a65797b609055bd50abb62f2b260d35ff
Ramon Wong <rcnwong78@gmail.com>
2025-02-03 16:48:08 +0000

    server running basic index.html

---
+++ main.js
@@ -0,0 +1,30 @@
+const express																= require('express');
+const session																= require('express-session');
+const path																	= require('path');
+
+const app			= express();
+const dataPath		= path.join(__dirname, 'data', 'index.json');
+const PORT			= 9100;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(session({
+		secret: 'your_secret_key',
+		resave: false,
+		saveUninitialized: true,
+		cookie: { maxAge: 15 * 60 * 1000 }								                                // 15 mins
+	}));
+
+// Serve static files
+app.use('/script',      express.static(path.join(__dirname, 'page/script')));
+app.use('/style',       express.static(path.join(__dirname, 'page/style')));
+app.listen(PORT, () => {console.log(`Server running on http://localhost:${PORT}`);});
+
+// index route
+app.get('/', (req, res) => {
+
+	if(!req.session.viewCount){	 req.session.viewCount	= 1; }else{	req.session.viewCount  += 1; }		// Ensure it's a number
+	res.sendFile(path.join(__dirname, 'page/index.html'));
+});
+
