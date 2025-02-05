0c8ad6690159cedd39c4520b44c4b27d892bd257
Ramon Wong <rcnwong78@gmail.com>
2025-02-05 00:15:35 +0000

    clean up

--- page/script/script.js
+++ page/script/script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 	const eventSource				= new EventSource('/events');
 	const rightMenu					= document.createElement('div');
-	const appMenu					= document.createElement('div');
+	const header					= document.createElement('div');
 
 	app.id							= 'app';
 	app.className					= ' ';
@@ -17,43 +17,28 @@ document.addEventListener('DOMContentLoaded', async () => {
 	rightMenu.id					= 'right-menu';
 	rightMenu.className				= 'vertical-menu';
 
-	appMenu.id						= 'app-Menu'
-	appMenu.className				= 'vertical-menu';
+	header.id						= 'header';
+	
 
-	const AppItems = [	{ id: 'appMenu-item-1', text: 'Item 1' },
-						{ id: 'appMenu-item-2', text: 'Item 2' },
-						{ id: 'appMenu-item-3', text: 'Item 3' },
-						{ id: 'appMenu-item-4', text: 'Item 4' }];
-
-	const menuItems = [	{ id: 'rightMenu-item-1', text: 'Item 1' },
-						{ id: 'rightMenu-item-2', text: 'Item 2' },
-						{ id: 'rightMenu-item-3', text: 'Item 3' },
-						{ id: 'rightMenu-item-4', text: 'Item 4' },
-						{ id: 'rightMenu-item-5', text: 'Item 5' },
-						{ id: 'rightMenu-item-6', text: 'Item 6' }];
+	const menuItems = [	{ id: 'rightMenu-item-1', text: 'Item 1', callback: () => { console.log('item clicked 1');} },
+						{ id: 'rightMenu-item-2', text: 'Item 2', callback: () => { console.log('item clicked 2');} },
+						{ id: 'rightMenu-item-3', text: 'Item 3', callback: () => { console.log('item clicked 3');} },
+						{ id: 'rightMenu-item-4', text: 'Item 4', callback: () => { console.log('item clicked 4');} },
+						{ id: 'rightMenu-item-5', text: 'Item 5', callback: () => { console.log('item clicked 5');} },
+						{ id: 'rightMenu-item-6', text: 'Item 6', callback: () => { console.log('item clicked 6');} }];
 
 	menuItems.forEach( item => {	
 		const link			= document.createElement('a');
 		link.id				= item.id;
 		link.href			= '#';
 		link.textContent	= item.text;
-		link.addEventListener('click', (e) => {		console.log(`clicked at ${item.id}`);});
+		link.addEventListener('click', (e) => {	 item.callback();});
 
 		rightMenu.appendChild(link);
 	});
 
-	AppItems.forEach( item => {
-		const link			= document.createElement('a');
-		link.id				= item.id;
-		link.href			= '#';
-		link.textContent	= item.text;
-		link.addEventListener('click', (e) => {		console.log(`clicked at ${item.id}`);});
-
-		appMenu.appendChild(link);
-	})
-
-	body.appendChild(appMenu);
 	body.appendChild(rightMenu);
+	body.appendChild(header);
 	body.appendChild(app);
 	
 	rightMenu.addEventListener('mouseleave', () => { rightMenu.style.display = 'none'; })
@@ -68,22 +53,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 		rightMenu.style.top			= `${y}px`;
 		rightMenu.style.display		= 'block'; // Show the menu
 	});
-
-	app.addEventListener('contextmenu', (e) => {
-		e.preventDefault();
-		e.stopPropagation();
-
-		const x = e.clientX;
-		const y = e.clientY;
-
-		console.log(`app at (${x}, ${y})`);
-		appMenu.style.left			= `${x}px`;
-		appMenu.style.top			= `${y}px`;
-		appMenu.style.display		= 'block'; // Show the menu
-	});
-
-	appMenu.addEventListener('mouseleave', () => { appMenu.style.display = 'none'; })
-
 
 	eventSource.onmessage = function(event){
 		const message = JSON.parse(event.data);
